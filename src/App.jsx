d30b6302a86a92be2b7cd7a5eace4250554c18fe
Emilse Arias <emilsearias@gmail.com>
2024-09-10 16:23:01 -0300

    agregamos una imagen usando import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import logo from './assets/logosimpson.png'
+
 function App() {
   
-
   return (
     <>
-     
+    <Container className='text-center my-4'>
+      <img src={logo} alt="Logo de los simpsons" className='w-50'/>
+
+    </Container>
     </>
   )
 }
