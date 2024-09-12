48066c41e6e3ff340df56eebf7b67aa0805ed809
Emilse Arias <emilsearias@gmail.com>
2024-09-12 17:14:44 -0300

    agregamos el login cuando carga la frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container } from 'react-bootstrap';
-import logo from './assets/logosimpson.png'
-import Frase from './components/Frase';
-import './App.css'
-import { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from "react-bootstrap";
+import logo from "./assets/logosimpson.png";
+import Frase from "./components/Frase";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Commet } from "react-loading-indicators";
 function App() {
-  const [personaje, setPersonaje] = useState({}) 
-  
-  useEffect(()=>{
-    consultarAPI();
-  }, [])
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarLoading, setMostrarLoading] = useState(true);
 
-  const consultarAPI = async()=>{
-    // FetchAPI
-    // solicitud GET (POST, PUT o PATCH, DELETE)
-    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
-    const datos = await respuesta.json()
-    console.log(datos[0])
-    setPersonaje(datos[0])
-  }
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    try {
+      setMostrarLoading(true);
+      // FetchAPI
+      // solicitud GET (POST, PUT o PATCH, DELETE)
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      //verifico si me llego la respuesta
+      if(respuesta.status === 200){
+        const datos = await respuesta.json();
+        setPersonaje(datos[0]);
+        //mostrar el componente
+        setMostrarLoading(false);
+      }
+    
+    } catch (error) {
+      console.error(error);
+      //mostrar un mensaje al usuario final
+    }
+   
+  };
 
   return (
     <>
-    <Container className='text-center my-4'>
-      <img src={logo} alt="Logo de los simpsons" className='w-50'/>
-      <Frase personaje={personaje}></Frase>
-      <Button variant='warning' className='mt-3' onClick={consultarAPI}>Obtener frase</Button>
-    </Container>
+      <Container className="text-center my-4">
+        <img src={logo} alt="Logo de los simpsons" className="w-50" />
+        {mostrarLoading ? (
+          <div>
+            <Commet color="#ffff" size="medium" text="" textColor="" />
+          </div>
+        ) : (
+          <Frase personaje={personaje}></Frase>
+        )}
+
+        <Button variant="warning" className="mt-3" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
