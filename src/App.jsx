e5792408eae73ac6776a930b154d548bf63067e1
Emilse Arias <emilsearias@gmail.com>
2024-09-12 15:18:08 -0300

    realizamos la solicitud a la api de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ import { Button, Container } from 'react-bootstrap';
 import logo from './assets/logosimpson.png'
 import Frase from './components/Frase';
 import './App.css'
+import { useEffect } from 'react';
 function App() {
-  
+    
+  useEffect(()=>{
+    consultarAPI();
+  }, [])
+
+  const consultarAPI = async()=>{
+    // FetchAPI
+    // solicitud GET (POST, PUT o PATCH, DELETE)
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    const datos = await respuesta.json()
+    console.log(datos)
+  }
+
   return (
     <>
     <Container className='text-center my-4'>
